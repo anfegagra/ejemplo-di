f0fc2f0767e568e5dc3d67cb9e2d545f20b9e69d
anfegagra <andres.granados@ceiba.com.co>
2020-02-19 23:04:29 -0500

    se agrega otra implementación de la interface y se pone @Primary para elegir el bean a inyectar

--- src/main/java/com/anfegagra/di/app/models/service/MiServicioImpl.java
+++ src/main/java/com/anfegagra/di/app/models/service/MiServicioImpl.java
@@ -1,12 +1,14 @@
 package com.anfegagra.di.app.models.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+@Primary
 @Service
 public class MiServicioImpl implements MiServicio {
 
 	@Override
 	public String hacerAlgo() {
-		return "Haciendo cualquier cosa!";
+		return "Haciendo cualquier cosa en bean1";
 	}
 }
---
+++ src/main/java/com/anfegagra/di/app/models/service/MiServicioImpl2.java
@@ -0,0 +1,12 @@
+package com.anfegagra.di.app.models.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MiServicioImpl2 implements MiServicio {
+
+	@Override
+	public String hacerAlgo() {
+		return "Haciendo cualquier cosa en bean2";
+	}
+}
